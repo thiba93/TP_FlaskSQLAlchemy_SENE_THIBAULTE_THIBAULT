0ed426d1b8b96667e018555efb5f6b99dc08b762
Mostapha <mossb.addi@gmail.com>
2024-02-13 16:32:38 +0100

    Créer route ['Film', 'User'] + Créer formulaire ajout film

--- src/critique_film/forms.py
+++ src/critique_film/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 class InscriptionForm(FlaskForm):
     nom = StringField('Nom', validators=[DataRequired()]) 
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired()]) 
     submit = SubmitField('Inscription')
+
+
+class AjoutFilmForm(FlaskForm):
+    titre = StringField('Titre', validators=[DataRequired()])
+    realisateur = StringField('Réalisateur')
+    annee_sortie = IntegerField('Année de sortie')
+    genre = StringField('genre')
+    submit = SubmitField('Submit')
--- src/critique_film/routes.py
+++ src/critique_film/routes.py
@@ -1,12 +1,50 @@
 from flask import Blueprint, render_template
-
+from .forms import InscriptionForm, AjoutFilmForm
+from .models import Utilisateur, Film
+from .database import db
+from werkzeug.security import generate_password_hash
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/<int:id>')
+
+@main.route('/inscription', methods=['GET', 'POST'])
+def inscription():
+    # Définir les actions pour inscrire un utilisateur
+    form = InscriptionForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.mot_de_passe.data)
+
+        # logique pour entrer l'utilisateur dans la BDD
+        new_user = Utilisateur(
+            nom=form.nom.data,
+            email=form.email.data,
+            mot_de_passe_hash = hashed_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('index.html')
+    return render_template('inscription.html', form=form)
+
+@main.route('/ajouter-film', methods=['GET', 'POST'])
+def ajouter_film():
+    form = AjoutFilmForm()
+    if form.validate_on_submit():
+        new_film = Film(
+            titre = form.titre.data,
+            realisateur = form.realisateur.data,
+            annee_sortie = form.annee_sortie.data,
+            genre = form.genre.data
+        )
+        db.session.add(new_film)
+        db.session.commit()
+        return render_template('index.html')
+    return render_template('ajouter-film.html', form=form)
+
+@main.route('/film/<int:id>')
 def display(id):
-    return render_template('display.html', id=id)
+    film = Film.query.get_or_404(id)
+    return render_template('afficher-film.html', film=film)
 
