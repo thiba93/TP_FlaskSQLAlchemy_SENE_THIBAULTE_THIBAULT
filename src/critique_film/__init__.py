b6b590b8df54c988faf215dac88d8fa10c615561
Mostapha <mossb.addi@gmail.com>
2024-02-13 11:38:23 +0100

    instanciation de migrate et initialisation + déclaration de DB dans un fichier séparé pour éviter un import circulaire

--- src/critique_film/__init__.py
+++ src/critique_film/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .models import Utilisateur
+from .database import db 
 
-db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -11,7 +13,9 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    
+
+    migrate.init_app(app, db)
+
     from .routes import main
     app.register_blueprint(main)
 
