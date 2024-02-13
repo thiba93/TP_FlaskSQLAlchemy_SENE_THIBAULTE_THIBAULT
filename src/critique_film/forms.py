9df6549d676a180f68e3db6df1b661f9affabf12
Mostapha <mossb.addi@gmail.com>
2024-02-13 13:37:06 +0100

    Création du formulaire inscription utilisateur

---
+++ src/critique_film/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class InscriptionForm(FlaskForm):
+    nom = StringField('Nom', validators=[DataRequired()]) 
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired()]) 
+    submit = SubmitField('Inscription')
