f726e434b52561f58372841daad4bbc8b1521917
Mostapha <mossb.addi@gmail.com>
2024-02-13 09:59:08 +0100

    Configuration de la BDD et connexion avec Flask

--- src/critique_film/__init__.py
+++ src/critique_film/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@<domaine|ip>:<port>/<database>'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@db/critiques_de_films'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    db.init_app(app)
+    
     from .routes import main
     app.register_blueprint(main)
 
