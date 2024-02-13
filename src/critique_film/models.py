0a1ec0a3775c263251251faa6a4eb67e2dc45dc0
Mostapha <mossb.addi@gmail.com>
2024-02-13 16:30:02 +0100

    Terminer Utilisateur Model + Ajouter Film Model

--- src/critique_film/models.py
+++ src/critique_film/models.py
@@ -2,6 +2,13 @@ from .database import db
 
 class Utilisateur(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # nom = 
-    # email = 
-    # mot_de_passe_hash = 
+    nom = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    mot_de_passe_hash = db.Column(db.String(500), nullable=False)
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(100), nullable=False)
+    realisateur = db.Column(db.String(100))
+    annee_sortie =  db.Column(db.Integer)
+    genre = db.Column(db.String(100))
