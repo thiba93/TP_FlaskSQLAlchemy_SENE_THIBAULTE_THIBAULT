6dc3dd5e4d849243c8f149ba02bc1bee5e70cbd2
Mostapha <mossb.addi@gmail.com>
2024-02-13 14:05:58 +0100

    Ajout de la SECRET_KEY dans la configuration de Flask pour CSRF

--- src/critique_film/__init__.py
+++ src/critique_film/__init__.py
@@ -3,12 +3,14 @@ from flask_migrate import Migrate
 from .models import Utilisateur
 from .database import db 
 
+
 migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@<domaine|ip>:<port>/<database>'
+    app.config['SECRET_KEY'] = 'mysecretkey'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@db/critiques_de_films'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
