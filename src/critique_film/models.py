bf7c4038bc0cedaa8a4082a1f35daf3e3c854eb9
Mostapha <mossb.addi@gmail.com>
2024-02-15 09:39:44 +0100

    Création des tables intermédiaires pour les likes avec db.Table

--- src/critique_film/models.py
+++ src/critique_film/models.py
@@ -1,6 +1,23 @@
 from .database import db
 from datetime import datetime
 from flask_login import UserMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+
+
+
+
+like_film = db.Table(
+    "like_film",
+    db.Column("id_film", db.Integer, db.ForeignKey('film.id')),
+    db.Column("id_utilisateur", db.Integer, db.ForeignKey('utilisateur.id'))
+)
+
+
+like_critique = db.Table(
+    "like_critique",
+    db.Column("id_critique", db.Integer, db.ForeignKey('critique.id')),
+    db.Column("id_utilisateur", db.Integer, db.ForeignKey('utilisateur.id'))
+)
 
 class Utilisateur(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,10 +25,14 @@ class Utilisateur(db.Model, UserMixin):
     email = db.Column(db.String(80), nullable=False, unique=True)
     mot_de_passe_hash = db.Column(db.String(500), nullable=False)
     critiques = db.relationship('Critique', backref='auteur', lazy='dynamic')
+    films_likes = db.relationship('Film', secondary=like_film, backref=db.backref('liked_by', lazy='dynamic'))
+    critiques_likes = db.relationship('Critique', secondary=like_critique, backref=db.backref('liked_by', lazy='dynamic'))
 
 # film = Film.query.get_or_404(id)
 # film.critiques 
     
+
+
 
 class Film(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +42,12 @@ class Film(db.Model):
     genre = db.Column(db.String(100))
     critiques = db.relationship('Critique', backref='film', lazy='dynamic')
 
+    @hybrid_property
+    def likes(self):
+        return len(self.liked_by.all())
+    
+
+
 
 
 class Critique(db.Model):
@@ -29,3 +56,9 @@ class Critique(db.Model):
     date_post = db.Column(db.DateTime, default=datetime.utcnow)
     id_utilisateur = db.Column(db.Integer, db.ForeignKey('utilisateur.id')) 
     id_film = db.Column(db.Integer, db.ForeignKey('film.id'))
+
+# class LikeFilm(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     id_utilisateur = db.Column(db.Integer, db.ForeignKey('utilisateur.id')) 
+#     id_film = db.Column(db.Integer, db.ForeignKey('film.id'))
+
